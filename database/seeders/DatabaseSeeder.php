91a9e2e4eecac28f08678cfb178b444452d06f71
Karl Ken. G. <formycodingonly@gmail.com>
2024-01-11 18:16:50 +0800

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            CategorySeeder::class,
+            UnitSeeder::class,
+            ProductSeeder::class
+        ]);
+
+        // Customer::factory(15)->create();
+        // Supplier::factory(15)->create();
     }
 }
